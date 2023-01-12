8dea2cd6d07914a3b21d9d8dd0a07a3a234c4686
Andreas Bandusch <andreas.bandusch@gmx.de>
2023-01-12 10:52:43 +0100

    Added routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { WelcomeComponent } from './components/welcome/welcome.component';
+import { SignUpComponent } from './components/sign-up/sign-up.component';
+import { LoggedWrapperComponent } from './components/logged-wrapper/logged-wrapper.component';
 
 const routes: Routes = [
   { path: 'welcome', component: WelcomeComponent },
+  { path: 'sign-up', component: SignUpComponent },
+  { path: 'chat', component: LoggedWrapperComponent }
 ];
 
 @NgModule({
