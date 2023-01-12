8eb7c988b5a6509bbe401caca02be7b911f440d4
Sneakz96 <Jahleelr-h@gmx.de>
2023-01-12 01:51:02 +0100

    add navbars

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,43 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SignUpComponent } from './components/sign-up/sign-up.component';
 import { WelcomeComponent } from './components/welcome/welcome.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { ImprintComponent } from './components/imprint/imprint.component';
+import { PrivacyComponent } from './components/privacy/privacy.component';
+import { ChannelsComponent } from './components/channels/channels.component';
+import { LoggedWrapperComponent } from './components/logged-wrapper/logged-wrapper.component';
+import { SideMenuComponent } from './components/side-menu/side-menu.component';
+
+import { MatIconModule } from '@angular/material/icon';
+import { MatTreeModule } from '@angular/material/tree';
+
+
 
 @NgModule({
   declarations: [
     AppComponent,
     SignUpComponent,
-    WelcomeComponent
+    WelcomeComponent,
+    NavbarComponent,
+    ImprintComponent,
+    PrivacyComponent,
+    ChannelsComponent,
+    LoggedWrapperComponent,
+    SideMenuComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    HttpClientModule,
+    MatIconModule,
+    MatTreeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/logged-wrapper/logged-wrapper.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-logged-wrapper',
+  templateUrl: './logged-wrapper.component.html',
+  styleUrls: ['./logged-wrapper.component.scss']
+})
+export class LoggedWrapperComponent {
+
+}
