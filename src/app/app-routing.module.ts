69039ec2346b8ab3319cd8a02b7ea9a6297d39dc
Kevin Wagner <kevin_wagner4493@web.de>
2023-02-08 17:52:40 +0100

    added imprint and privacy

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,12 +5,16 @@ import { SignUpComponent } from './components/sign-up/sign-up.component';
 import { LoggedWrapperComponent } from './components/logged-wrapper/logged-wrapper.component';
 import { LoginComponent } from './components/login/login.component';
 import { ChannelDetailComponent } from './components/channel-detail/channel-detail.component';
+import { ImprintComponent } from './components/imprint/imprint.component';
+import { PrivacyComponent } from './components/privacy/privacy.component';
 
 const routes: Routes = [
   { path: '', component: WelcomeComponent, pathMatch: 'full' },
   { path: 'welcome', component: WelcomeComponent },
   { path: 'sign-up', component: SignUpComponent },
   { path: 'login', component: LoginComponent },
+  { path: 'imprint', component: ImprintComponent },
+  { path: 'privacy', component: PrivacyComponent },
   {
     path: 'chat', component: LoggedWrapperComponent, children: [
       { path: 'channel/:id', component: ChannelDetailComponent }
